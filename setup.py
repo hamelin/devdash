beb34257a323eadda816d914866b5ebdcb053a9d
Benoit Hamelin <benoit.hamelin@cyber.gc.ca>
2021-12-23 20:51:19 +0000

    Further prep setup.py for release

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
 from setuptools import setup, find_packages
 
+
 setup(
     name="devdash",
     version="0.9",
     author="Benoit Hamelin",
     email="benoit@benoithamelin.com",
     description="Automatic developer dashboard",
+    license="MIT",
+    url="https://github.com/hamelin/devdash",
     packages=find_packages(),
-    install_requires=["ipywidgets", "ipython", "watchdog"]
+    install_requires=["ipywidgets", "ipython", "watchdog"],
+    keywords=["Development", "Dashboard", "Jupyter"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Developer Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10"
+    ]
 )
