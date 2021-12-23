1446c8eeeb9a2d3699464a0d24048b5649771793
Benoit Hamelin <benoit.hamelin@cyber.gc.ca>
2021-12-23 20:44:57 +0000

    Set initial release version at 0.9
    This marks that this is beta-quality software, ready for testing by the
    masses, but likely to require fixes.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="devdash",
-    version="1.0.0",
+    version="0.9",
     author="Benoit Hamelin",
     email="benoit@benoithamelin.com",
     description="Automatic developer dashboard",
